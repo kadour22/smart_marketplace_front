e7b282fd8a5fac4f76c03196ace4de5ff07f0a90
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-28 15:35:54 +0100

    setup Listing products logic for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 import ProductsList from './components/products/ProductsList'
 import Login from './components/User/Login'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ProductsList/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{use, useEffect,useState} from 'react'
+import { getProductsList } from './services/products_services'
 
 const ProductsList = () => {
-  return (
-    <div>ProductsList</div>
+    const [products, setProducts] = useState([]);
+    // console.log(localStorage.getItem("access_token"))
+    useEffect(()=>{
+        const fetchProducts = async () => {
+            try{
+                const response = await getProductsList();
+                
+                setProducts(response);
+            }catch(error){
+                console.error("Error in fetching products inside component:", error);
+            }
+        }
+        fetchProducts();
+    },[])
+    return (
+    <div>
+        <h2>Products List</h2>
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>{product.product_name} - ${product.price}</li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
